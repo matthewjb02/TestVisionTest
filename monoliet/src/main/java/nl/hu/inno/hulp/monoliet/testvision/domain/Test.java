eefffda997b80c9c084418f331f6407dfe28f65f
Ruben <rubenstryder@gmail.com>
2024-09-14 21:14:08 +0200

    Commenting getQuestionsAsString

--- monoliet/src/main/java/nl/hu/inno/hulp/monoliet/testvision/domain/Test.java
+++ monoliet/src/main/java/nl/hu/inno/hulp/monoliet/testvision/domain/Test.java
@@ -8,7 +8,6 @@ import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Entity
 public class Test {
