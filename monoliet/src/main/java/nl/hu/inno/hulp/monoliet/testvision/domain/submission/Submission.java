3f5c3e0aa5a7b3a308c777975bbbcc666cf8fa33
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2024-09-17 21:48:19 +0200

    changes

--- monoliet/src/main/java/nl/hu/inno/hulp/monoliet/testvision/domain/submission/Submission.java
+++ monoliet/src/main/java/nl/hu/inno/hulp/monoliet/testvision/domain/submission/Submission.java
@@ -50,8 +50,8 @@ public class Submission {
     public void updateGradingForQuestion(int questionNr, int givenPoints, String feedback) {
         Question question = exam.seeQuestion(questionNr);
         if (question != null) {
-            if(givenPoints > question.getPoints()) {
-                throw new IllegalArgumentException("Given points cannot be higher than the total points for the question");
+            if(givenPoints > question.getPoints() || givenPoints < 0) {
+                throw new IllegalArgumentException("Given points must be between 0 and the maximum points of the question");
             }
             question.addGivenPoints(givenPoints);
             question.addTeacherFeedback(feedback);
