c161bc3281fe3e99ebd64bfcea5f7f912c6ebe7a
Ruben <rubenstryder@gmail.com>
2024-09-29 03:26:05 +0200

    Added exam session service and exam date

---
+++ monoliet/src/main/java/nl/hu/inno/hulp/monoliet/testvision/application/service/ExamSessionService.java
@@ -0,0 +1,9 @@
+package nl.hu.inno.hulp.monoliet.testvision.application.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class ExamSessionService {
+}
