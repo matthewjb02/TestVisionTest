81273314688ce67afd90d10319763d66c2901142
Ruben <rubenstryder@gmail.com>
2024-09-30 04:50:52 +0200

    Splitting up examination and exam session

--- monoliet/src/main/java/nl/hu/inno/hulp/monoliet/testvision/domain/examination/Examination.java
+++ monoliet/src/main/java/nl/hu/inno/hulp/monoliet/testvision/domain/examination/Examination.java
@@ -2,9 +2,6 @@ package nl.hu.inno.hulp.monoliet.testvision.domain.examination;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import nl.hu.inno.hulp.monoliet.testvision.domain.question.MultipleChoiceQuestion;
-import nl.hu.inno.hulp.monoliet.testvision.domain.question.OpenQuestion;
-import nl.hu.inno.hulp.monoliet.testvision.domain.question.Question;
 import nl.hu.inno.hulp.monoliet.testvision.domain.exam.Exam;
 import nl.hu.inno.hulp.monoliet.testvision.domain.user.Student;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,9 +15,6 @@ public class Examination {
     @GeneratedValue
     private Long id;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private Student student;
-
     @OneToMany(cascade = CascadeType.ALL)
     private List<Student> candidates;
 
@@ -30,8 +24,7 @@ public class Examination {
     @OneToOne(cascade = CascadeType.ALL)
     private Exam exam;
 
-    private ExamState state;
-
+    private String name;
     private String password;
 
     @Embedded
@@ -43,41 +36,13 @@ public class Examination {
     protected Examination() {
     }
 
-    public Examination(Student student, Exam exam) {
-        this.student = student;
-        this.exam = exam;
-        this.state = ExamState.Active;
-    }
-
-    public Examination(Exam exam, String password, ExamDate examDate, int duration, int extraTime) {
+    public Examination(String name, Exam exam, String password, ExamDate examDate, int duration, int extraTime) {
+        this.name = name;
         this.exam = exam;
         this.password = password;
         this.examDate = examDate;
         this.duration = duration;
         this.extraTime = extraTime;
-    }
-
-    public Question seeQuestion(int nr) {
-        return exam.getQuestions().get(nr - 1);
-    }
-
-    public void answerQuestion(int questionNr, Object answer) {
-        Question question = seeQuestion(questionNr);
-
-        System.out.println(question.getClass());
-
-        if (question.getClass().equals(MultipleChoiceQuestion.class)){
-            MultipleChoiceQuestion mcQuestion = (MultipleChoiceQuestion)question;
-            mcQuestion.setAnswer((int)answer);
-        } else {
-            OpenQuestion openQuestion = (OpenQuestion)question;
-            openQuestion.setAnswer((String) answer);
-        }
-    }
-
-    public Examination endExam() {
-        this.state = ExamState.Completed;
-        return this;
     }
 
     public int totalDuration(boolean extraTimeRight) {
@@ -86,6 +51,11 @@ public class Examination {
         }
 
         return duration;
+    }
+
+    public Examination selectCandidates(List<Student> candidates) {
+        this.candidates = candidates;
+        return this;
     }
 
     public List<ExamSession> setupExamSessions() {
@@ -101,6 +71,10 @@ public class Examination {
         examSessions.add(examSession);
     }
 
+    public boolean validateStudent(Student student) {
+        return candidates.contains(student);
+    }
+
     public String hashPassword(String password) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         return passwordEncoder.encode(password);
@@ -108,9 +82,5 @@ public class Examination {
 
     public void changePassword(String newPassword) {
         this.password = newPassword;
-    }
-
-    public Long getStudentId() {
-        return student.getId();
     }
 }
