5cb0cc550d3df0d5bb29d6a12fc44ee2625c24f7
rik-prins <rik-prins@hotmail.com>
2024-10-07 10:26:31 +0200

    iets vergeten

--- monoliet/src/main/java/nl/hu/inno/hulp/monoliet/testvision/domain/examination/ExamSession.java
+++ monoliet/src/main/java/nl/hu/inno/hulp/monoliet/testvision/domain/examination/ExamSession.java
@@ -10,6 +10,8 @@ import nl.hu.inno.hulp.monoliet.testvision.domain.question.OpenQuestion;
 import nl.hu.inno.hulp.monoliet.testvision.domain.question.Question;
 import nl.hu.inno.hulp.monoliet.testvision.domain.question.QuestionEntity;
 import nl.hu.inno.hulp.monoliet.testvision.domain.user.Student;
+
+import java.util.List;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
@@ -61,7 +63,7 @@ public class ExamSession {
 
         if (question.getClass().equals(MultipleChoiceQuestion.class)){
             MultipleChoiceQuestion mcQuestion = (MultipleChoiceQuestion)question;
-            mcQuestion.setAnswer((int)answer);
+            mcQuestion.setGivenAnswers((List<Integer>)answer);
         } else {
             OpenQuestion openQuestion = (OpenQuestion)question;
             openQuestion.setAnswer((String) answer);
