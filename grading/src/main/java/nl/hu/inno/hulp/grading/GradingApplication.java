d39c94147d8bcfcdeaea6be91c68180cfd20cd08
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2024-11-24 14:15:43 +0100

    deployment grading?

--- grading/src/main/java/nl/hu/inno/hulp/grading/GradingApplication.java
+++ grading/src/main/java/nl/hu/inno/hulp/grading/GradingApplication.java
@@ -1,10 +1,20 @@
 package nl.hu.inno.hulp.grading;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GradingApplication {
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @PostConstruct
+    public void logDatasourceUrl() {
+        System.out.println("Configured Datasource URL: " + datasourceUrl);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GradingApplication.class, args);
