2ee84944b5784ceb6270814314c637e7baf16c6e
matthewbijlhout <matthew.bijlhout@student.hu.nl>
2024-11-25 03:07:17 +0100

    examination deployment?

--- grading/src/main/java/nl/hu/inno/hulp/grading/GradingApplication.java
+++ grading/src/main/java/nl/hu/inno/hulp/grading/GradingApplication.java
@@ -7,15 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GradingApplication {
-
-    @Value("${spring.datasource.url}")
-    private String datasourceUrl;
-
-    @PostConstruct
-    public void logDatasourceUrl() {
-        System.out.println("Configured Datasource URL: " + datasourceUrl);
-    }
-
+    
     public static void main(String[] args) {
         SpringApplication.run(GradingApplication.class, args);
     }
